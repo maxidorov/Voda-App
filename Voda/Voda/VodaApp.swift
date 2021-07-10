dcbfce96e1ef8e3ccea7e68dfd208145f91342f9
Maxim <s_maxim_v@bk.ru>
2021-07-10 18:49:38 +0300

    Init project

---
+++ Voda/Voda/VodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VodaApp.swift
+//  Voda
+//
+//  Created by Maxim V. Sidorov on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct VodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
